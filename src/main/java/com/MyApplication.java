db55a04537511221bee8eb8510a4b11a07c4066c
ChrisRyo <chrisryo0301@gmail.com>
2016-02-17 14:54:57 +0800

    test

---
+++ src/main/java/com/MyApplication.java
@@ -0,0 +1,11 @@
+package com;
+
+import javax.ws.rs.ApplicationPath;
+import org.glassfish.jersey.server.ResourceConfig;
+ 
+@ApplicationPath("rest")
+public class MyApplication extends ResourceConfig{
+    public MyApplication(){
+        packages("com.my");
+    }
+}
